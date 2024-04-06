ccdc0e36b82a9c06847a13c4632e193492f58796
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-04-06 16:27:45 +0300

    optimize imports in MainActivity

--- android/app/src/main/java/hackathon/okrugyuga/MainActivity.kt
+++ android/app/src/main/java/hackathon/okrugyuga/MainActivity.kt
@@ -1,18 +1,9 @@
 package hackathon.okrugyuga
 
 import android.os.Bundle
-import android.view.ViewGroup
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
 import com.yandex.mapkit.MapKitFactory
-import com.yandex.mapkit.mapview.MapView
-import hackathon.okrugyuga.ui.theme.OkrugYugaTheme
 
 
 class MainActivity : ComponentActivity() {
